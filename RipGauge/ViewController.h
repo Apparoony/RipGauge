1cf4016e90fb8bebc711186ba6f2f330af4bbf27
bigjimcfh <bigjimcfh@gmail.com>
2017-11-06 00:03:06 -0700

    Initial Commit

---
+++ RipGauge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RipGauge
+//
+//  Created by jmhillak on 11/6/17.
+//  Copyright © 2017 Apparoony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RipGauge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RipGauge
+//
+//  Created by jmhillak on 11/6/17.
+//  Copyright © 2017 Apparoony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
